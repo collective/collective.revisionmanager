13f934a12fc0bb0d8f70cac1134a5461763a5ebc
tschorr <t_schorr@gmx.de>
2016-03-01 11:11:25 +0100

    new version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = (
 
 setup(
     name='collective.revisionmanager',
-    version='0.1',
+    version='0.2.dev0',
     description="Manage CMFEdition Histories",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
