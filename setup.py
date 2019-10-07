3707277ebe07010774c70c869821581695958e3a
tschorr <t_schorr@gmx.de>
2019-10-07 09:36:33 +0200

    Back to development: 1.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = (
 
 setup(
     name='collective.revisionmanager',
-    version='0.9',
+    version='1.0.dev0',
     description="Manage CMFEditions Histories",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
